d83c28d400c06bab446d2c53e7dc3c53a8ba331e
ASHWIN S <111366481+ashwinshaji@users.noreply.github.com>
2023-02-24 14:47:59 +0530

    Add files via upload

---
+++ ALD.java
@@ -0,0 +1,16 @@
+class ALD
+{
+public static void main(String args[])
+{
+ArrayList<string> a1=new Arraylist<string> ();
+a1.add("A");
+a1.add("D");
+a1.add(null);
+for(string a:a1){System.out.println(a);}
+a1.add(1,"Ashwin");
+for(string a:a1){System.out.println(a);}
+a1.remove("A");
+for(string a:a1){System.out.println(a);}
+System.out.println(a1.indexof("Ashwin"));
+}
+}
